9ef36d85aad9a4ac9ed2975dac95a88a1fe768b8
Williams <dawarland@gmail.Com>
2020-08-25 01:20:13 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express")
+const Expo = require("expo-server-sdk").default
+const cors = require("cors")
+
+const expo = new Expo()
+const expressServer = express();
+
+expressServer.use(cors())
+expressServer.listen(process.env.PORT || 3000, () => {
+    console.log("Serveur en ecoute sur "+ (process.env.PORT || 3000 ) )
+    expressServer.get("/", function(req, res) {
+        const token = req.query.token;
+        if(!Expo.isExpoPushToken()){
+            console.log("Erreur d'envoie");
+            res.send( {err : "Erreur d'envoie"} )
+        }else{
+            let messages = [
+                {
+                    to: token,
+                    sound: 'default',
+                    body : 'Notification test',
+                    data : { test : 'asfrdh'}
+                }
+            ]
+
+            expo.sendPushNotificationsAsync(messages).then( ticket => {
+                res.send( { ticket : ticket} )
+            }).catch( err => {
+                console.log("Erreur d'envoie");
+            res.send( {err : "Erreur d'envoie"} )
+            } )
+        }
+    })
+});
